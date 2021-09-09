545b3eabd5728cf4f1c9e111bcad849d50d20f37
Francisco Lima <juniorlima.dev@gmail.com>
2021-09-09 07:05:14 -0300

    Criado método deletar curso

--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -23,11 +23,11 @@
                             <td>{{$registro->id}}</td>
                             <td>{{$registro->titulo}}</td>
                             <td>{{$registro->descricao}}</td>
-                            <td><img width="120" src="{{asset($registro->imagem)}}" alt="{{$registro->titulo}}"></td>
+                            <td><img height="60" src="{{asset($registro->imagem)}}" alt="{{$registro->titulo}}"></td>
                             <td>{{$registro->publicado}}</td>
                             <td>
                                 <a href="{{route('admin.cursos.editar', $registro->id)}}" class="btn deep-orange">Editar</a>
-                                <a href="{{route('admin.cursos.deletar', $registro->id)}}" class="btn red">Excluir</a>
+                                <a href="{{route('admin.cursos.deletar', $registro->id)}}" class="btn red" onclick="return confirm('Deseja excluir este curso?');">Excluir</a>
                             </td>
                         </tr>
                     @endforeach
